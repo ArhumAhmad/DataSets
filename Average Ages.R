9020d83da77ea455d08c6f96c2bfb510d51d20be
ArhumAhmad <nhlfan1001@gmail.com>
2021-07-23 10:09:56 -0400

    created Averages.R

---
+++ Average Ages.R
@@ -0,0 +1,10 @@
+ages <- c()
+
+for(i in 1:nrow(City)){
+    city_data <- city_data <- Cab_Data[Cab_Data$City %in% City$City[i],]
+    users     <- unique(city_data$`Customer ID`)
+    data      <- Cust_ID_Red[Cust_ID_Red$`Customer ID` %in% t(users),]
+    age       <- mean(data$Age)
+
+    ages <- c(ages, age)
+}
